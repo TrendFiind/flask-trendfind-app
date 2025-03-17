983fa09613dea4c39d2aa4223e5ab760e72d0e7c
TrendFiind <eggo10k@gmail.com>
2025-03-17 13:07:06 +0800

    Update app.py

--- TrendFind/app.py
+++ TrendFind/app.py
@@ -99,4 +99,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Heroku's PORT or default to 5000
+    app.run(host="0.0.0.0", port=port)  # Run on all available IPs
