972b5cc30ec6d68efe2dd8158e86c16debc9d5bb
TrendFiind <eggo10k@gmail.com>
2025-04-18 11:12:52 +0800

    Update forms.py

--- TrendFind/forms.py
+++ TrendFind/forms.py
@@ -3,6 +3,19 @@ from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length
 
 class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=50)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    message = TextAreaField('Message', validators=[DataRequired(), Length(min=10, max=1000)])
+    name = StringField('Name', validators=[
+        DataRequired(),
+        Length(min=2, max=50)
+    ])
+    email = StringField('Email', validators=[
+        DataRequired(),
+        Email(),
+        Length(max=120)
+    ])
+    subject = StringField('Subject', validators=[
+        Length(max=100)
+    ])
+    message = TextAreaField('Message', validators=[
+        DataRequired(),
+        Length(min=10, max=2000)
+    ])
