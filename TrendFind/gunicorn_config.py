1160c10350797783b6a44ffeeab246dc93e53ffc
TrendFiind <eggo10k@gmail.com>
2025-06-09 04:32:23 +0800

    Update gunicorn_config.py

--- TrendFind/gunicorn_config.py
+++ TrendFind/gunicorn_config.py
@@ -1,5 +1,5 @@
-# gunicorn_config.py
-workers = 2
-bind = "0.0.0.0:$PORT"
-worker_class = "gevent"
+bind = "0.0.0.0:8000"
+workers = 3             # at least (2 × CPU) + 1 in prod
+worker_class = "gthread"
+threads = 2
 timeout = 30
