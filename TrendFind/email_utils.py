a0e780628dc0f4524f1670b101ab304372ba1ea4
TrendFiind <eggo10k@gmail.com>
2025-06-09 04:30:58 +0800

    Update email_utils.py

--- TrendFind/email_utils.py
+++ TrendFind/email_utils.py
@@ -1,1 +1,13 @@
+from flask_mail import Message
+from . import mail, celery
+from flask import current_app, render_template
 
+@celery.task
+def send_welcome_email(user_email, user_name):
+    msg = Message(
+        subject="Welcome to TrendFind",
+        recipients=[user_email],
+        sender=current_app.config["MAIL_USERNAME"]
+    )
+    msg.body = render_template("emails/welcome.txt", name=user_name)
+    mail.send(msg)
