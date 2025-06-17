de012ce63ba24e58868f385766f80736b49a1cc9
TrendFiind <eggo10k@gmail.com>
2025-06-17 18:30:56 +0800

    Update responsive.js

--- TrendFind/static/js/responsive.js
+++ TrendFind/static/js/responsive.js
@@ -1,1 +1,8 @@
+// static/js/responsive.js
+function updateScaling() {
+  const scaleFactor = window.innerWidth < 768 ? 0.95 : 1.25;
+  document.documentElement.style.setProperty('--scale-factor', scaleFactor);
+}
 
+window.addEventListener('resize', updateScaling);
+updateScaling();
