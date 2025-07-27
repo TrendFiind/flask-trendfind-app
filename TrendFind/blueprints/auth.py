fed03794bacadcec6a4a92615d6dcfd39df3b240
TrendFiind <eggo10k@gmail.com>
2025-07-27 20:55:19 +0800

    Update auth.py

--- TrendFind/blueprints/auth.py
+++ TrendFind/blueprints/auth.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request, jsonify, session
 from flask_login import login_user, logout_user, login_required, current_user
-from app.forms import RegisterForm, LoginForm
-from app.models import User, db
-from app.email_utils import send_welcome_email
+from forms import RegisterForm, LoginForm
+from models import User, db
+from email_utils import send_welcome_email
 
 # 🔐 Firebase Admin
 import firebase_admin
